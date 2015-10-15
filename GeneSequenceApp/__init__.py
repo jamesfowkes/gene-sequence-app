d72657302c59eb3b9e1d31ac9094b7052da44fd5
mellchie <missmichellestrickland@gmail.com>
2015-10-15 17:17:10 +0100

    Added default configuration file

--- GeneSequenceApp/__init__.py
+++ GeneSequenceApp/__init__.py
@@ -1,6 +1,9 @@
 from flask import Flask
 app=Flask(__name__)
 
+app.config.from_object("GeneSequenceApp.default")
+#app.config.from_envvar("GENESEQUENCEAPP_CFG")
+
 import GeneSequenceApp.views.content
 
 import GeneSequenceApp.controllers.sequence
